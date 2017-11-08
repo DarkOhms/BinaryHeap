ef9102d3a76c772a1adc86b40be6ab5b75ca8608
Luke Martin <luke_martin45@yahoo.com>
2017-11-07 23:59:34 -0800

    first commit

---
+++ src/BinaryHeap.java
@@ -0,0 +1,28 @@
+
+public class BinaryHeap {
+  
+  int size;
+  int index;
+  int endIndex;
+  int[] heap = new int[size];
+  
+  BinaryHeap(int size){
+	  this.size = size;
+	  this.index = 1;
+  }
+  
+  public void insert(int data){
+	  percolate(heap, );
+  }
+  
+  private void percolate(int[] a, int i, int n) {
+	  
+	  int child;
+	  int tmp;
+	  for(tmp = a[i]; a[2*i] < n ; i = child){
+		  
+	  }
+	  
+  }
+  
+}
